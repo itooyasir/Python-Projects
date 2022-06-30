7833ae02556d41a88ff3410114d654b6c157656f
itoo yasir <64364196+itooyasir@users.noreply.github.com>
2022-06-30 10:20:07 +0530

    Update phdetails.py

--- phdetails.py
+++ phdetails.py
@@ -10,7 +10,7 @@ from phonenumbers import carrier
 from phonenumbers import geocoder
 from phonenumbers import timezone
 
-number = "+917780874834"
+number = "+917780874866"
 number = ph.parse(number)
 print(timezone.time_zones_for_number(number))
 print(carrier.name_for_number(number, "en"))
